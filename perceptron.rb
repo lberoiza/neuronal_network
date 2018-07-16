9dcaa81e3c74fca6340086f20d1aaeee00bf3f5e
lberoiza <luis.beroiza@hotmail.com>
2018-07-16 22:29:58 +0200

    Implement the calculation of input abd weight
    Every neuron must calculate a value.
    This is a sum of all the entries multiplied by their respective weights

--- perceptron.rb
+++ perceptron.rb
@@ -10,10 +10,37 @@ class Perceptron
 		self.input_number = input_number # Número de parámetros de entrada
     
     	# Seleccionamos pesos aleatorios
-    	milliseconds = (Time.now.to_f-Time.now.to_i)*1000 
-	    self.w = Random.new(milliseconds).rand(100)
+	    self.w = []
+	    
+	    # inicializando array con los pesos de forma aleatoria
+	    1.upto(input_number) do
+    		milliseconds = (Time.now.to_f-Time.now.to_i)*1000 	    
+	    	self.w << Random.new(milliseconds).rand(100) 
+	 	end
+
 	    self.step_size = step_size # La tasa de convergencia
 
 	end
+
+
+	# Calcula los pesos de cada una de las entradas a un perceptron
+	def predict(inputs)
+        # Producto punto de entrada y pesos
+        weighted_average = 0
+
+        # genera un array con subarray formados por los elementos
+        #  entre los pesos y las entradas
+        self.w.zip(inputs).each do |pair|
+        	# calcula el producto peso y entrada y lo almacena en 'weighted_average'
+        	weighted_average += pair.reduce(:*)
+        end
+
+        if weighted_average > 0:
+            return 1
+        return 0
+    end
+
+
+
 
 end
